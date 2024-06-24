bc533fa0cdea801a7b2e596ad43c4fc28634b380
Cizaii <zang@dromara.org>
2024-06-24 22:50:50 +0800

    refactor(build): move dependencies from ai-sdk to parent pom
    Moved common dependencies from the ai-sdk module's pom.xml to the
    parent pom.xml to ensure dependency consistency across modules.

---
+++ inference/src/main/java/org/zang/ShortLinkAdminApplication.java
@@ -0,0 +1,20 @@
+package org.zang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Eliauk，微信：Cizai_，邮箱：zang.dromara.org <br/>
+ * @date 2024/6/24 <br/>
+ * @Copyright 博客：https://eliauku.gitee.io/  ||  per aspera and astra <br/>
+ */
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+@SpringBootApplication
+
+public class InferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InferenceApplication.class, args);
+    }
+}
