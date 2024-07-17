044e1b16d8c6c3c12a634c240a8bc8a4f7a04ddd
Cizaii <zang@dromara.org>
2024-07-17 21:50:56 +0800

    update

---
+++ inference/src/main/java/org/zang/processor/IeInferSubject.java
@@ -0,0 +1,21 @@
+package org.zang.processor;
+
+import lombok.Data;
+
+/**
+ * @author Eliauk，微信：Cizai_，邮箱：zang.dromara.org <br/>
+ * @date 2024/6/26 <br/>
+ * &#064;Copyright  博客：<a href="https://eliauku.gitee.io/">...</a>  ||  per aspera and astra <br/>
+ */
+@Data
+public class IeInferSubject {
+
+    /**
+     * 值
+     */
+    private String value;
+    /**
+     * 类型
+     */
+    private String type;
+}
