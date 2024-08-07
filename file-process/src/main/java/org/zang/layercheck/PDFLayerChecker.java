274de3103d5950632d86541bab29098e3d9bab93
Cizaii <zang@dromara.org>
2024-08-07 13:37:01 +0800

    ```移除无用的ChatMetadataRespDTO类，添加PDFLayerChecker类用于检测单层PDF移除在org.zang.dto.resp包下未使用的ChatMetadataRespDTO类。 在org.zang.layercheck包下添加PDFLayerChecker类，该类包含一个静态方法isSingleLayerPDF， 用于检查提供的PDF文件是否是单层的。这一改动优化了代码库， 确保所有类都是必要的，并且增加了PDF层次检查的功能。 ```

---
+++ file-process/src/main/java/org/zang/layercheck/PDFLayerChecker.java
@@ -0,0 +1,28 @@
+package org.zang.layercheck;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+/**
+ * @author Eliauk，微信：Cizai_，邮箱：zang.dromara.org <br/>
+ * @date 2024/6/26 <br/>
+ * &#064;Copyright  博客：<a href="https://eliauku.gitee.io/">...</a>  ||  per aspera and astra <br/>
+ */
+public class PDFLayerChecker {
+
+    public static boolean isSingleLayerPDF(File file) {
+        try (PDDocument document = Loader.loadPDF(file)) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            String text = stripper.getText(document);
+            return text.trim().isEmpty();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
